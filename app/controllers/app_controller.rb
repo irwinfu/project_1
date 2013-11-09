697cbaa2f88970b3680e127e47cf3b2ba4600955
Your Name <irwinfu@gmail.com>
2013-11-09 11:46:21 -0800

    Initial commit

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,2 +1,21 @@
 class AppController < ApplicationController
+
+  
+
+  def index
+  	@app = App.where("month = ? AND year = ?", params[:month], params[:year])
+  	render:json => @app
+  end
+
+  def create
+  	@app = App.new(app_params)
+  	@app.save
+  	render :nothing => true
+  end
+
+  private
+
+  def app_params
+  	params.require(:apps).permit(:event, :day, :time, :month, :year)
+  end
 end
